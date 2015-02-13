66869e1b2d7b9386ab2477a4aacf947c56b8e3e2
Matrix Ingram <matrix@clevercodename.com>
2015-02-13 04:57:28 -0800

    Initial commit.

---
+++ lib/sagn/server.rb
@@ -0,0 +1,7 @@
+require "sagn/server/version"
+
+module Sagn
+  module Server
+    # Your code goes here...
+  end
+end
---
+++ lib/sagn/server/version.rb
@@ -0,0 +1,5 @@
+module Sagn
+  module Server
+    VERSION = "0.0.1"
+  end
+end
